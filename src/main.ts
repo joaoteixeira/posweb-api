7431a78c798c2b8857b29f8ee36b785f08664ee4
João Teixeira <joao.teixeira@ifro.edu.br>
2022-10-23 09:44:20 -0400

    config de erros - validator

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
+
+  // Configuração de Erros com Detalhes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        return new BadRequestException(errors);
+      },
+      validationError: {
+        target: false,
+        value: true,
+      },
+    }),
+  );
+
 
   await app.listen(3000);
 }
